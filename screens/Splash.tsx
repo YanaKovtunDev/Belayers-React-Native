a402470fff0af45e241a908666e91f88490dc713
YanaKovtunDev <yana.kovtun.dev@gmail.com>
2023-12-22 15:14:46 +0200

    refactor: create component for each svg image

--- screens/Splash.tsx
+++ screens/Splash.tsx
@@ -1,36 +1,17 @@
 import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import Svg, { Path } from 'react-native-svg';
 import React from 'react';
 
 import { Typography, Container } from '../styles';
+import { TriangleBrown } from '../assets/icons/TriangleBrown';
+import { TriangleYellow } from '../assets/icons/TriangleYellow';
+import { TriangleGreen } from '../assets/icons/TriangleGreen';
+import { TriangleOrange } from '../assets/icons/TriangleOrange';
 
 export const SplashScreen = () => {
   return (
     <SafeAreaView style={[Container.screenContainer, styles.container]}>
-      <Svg
-        width="107"
-        height="47"
-        viewBox="0 0 107 47"
-        fill="none"
-        style={{ position: 'absolute', right: 80, top: 40 }}
-      >
-        <Path
-          d="M37.3006 46.6168L106.762 17.3953L0.00037967 0.946332L37.3006 46.6168Z"
-          fill="#83512A"
-        />
-      </Svg>
-      <Svg
-        width="154"
-        height="175"
-        viewBox="0 0 154 175"
-        fill="none"
-        style={{ position: 'absolute', right: 0, top: 20 }}
-      >
-        <Path
-          d="M258.108 -5.94127L-0.000123372 83.2064L157.418 174.315L258.108 -5.94127Z"
-          fill="#F4B33D"
-        />
-      </Svg>
+      <TriangleBrown />
+      <TriangleYellow />
       <View style={styles.container}>
         <Image source={require('../assets/logo.png')} style={{ marginBottom: 50 }} />
         <Text
@@ -43,27 +24,8 @@ export const SplashScreen = () => {
         </Text>
         <Text style={[Typography.header, { textAlign: 'center' }]}>Climbing is a lifestyle</Text>
       </View>
-      <Svg
-        width="109"
-        height="84"
-        viewBox="0 0 109 84"
-        fill="none"
-        style={{ position: 'absolute', left: 3, bottom: 60 }}
-      >
-        <Path
-          d="M32.0542 83.3522L108.554 43.0155L0.679426 0.594875L32.0542 83.3522Z"
-          fill="#A0BEAB"
-        />
-      </Svg>
-      <Svg
-        width="260"
-        height="115"
-        viewBox="0 0 260 115"
-        fill="none"
-        style={{ position: 'absolute', left: 0, bottom: 0 }}
-      >
-        <Path d="M44.9854 216.488L-117.831 120.186L260 0.435903L44.9854 216.488Z" fill="#D95721" />
-      </Svg>
+      <TriangleGreen />
+      <TriangleOrange />
     </SafeAreaView>
   );
 };
